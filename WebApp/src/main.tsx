aca6c2df66e9d1b70fac3699e5d9148aff4aa91b
Tadporn <64070153@kmitl.ac.th>
2023-06-11 15:53:01 +0700

    from cmd

--- WebApp/src/main.tsx
+++ WebApp/src/main.tsx
@@ -1,14 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryCLient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryCLient}>
     <App />
   </QueryClientProvider>
-
 );
